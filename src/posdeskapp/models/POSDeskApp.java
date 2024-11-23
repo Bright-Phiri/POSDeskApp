3074726628206035839163c0f7f642f0e1d8e7e8
Bright Issah <bphiri.aki@gmail.com>
2024-11-23 22:51:56 +0200

    feat: Add sells page

--- src/posdeskapp/models/POSDeskApp.java
+++ src/posdeskapp/models/POSDeskApp.java
@@ -9,6 +9,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 /**
@@ -21,9 +22,12 @@ public class POSDeskApp extends Application {
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/posdeskapp/views/Main.fxml"));
         
-        Scene scene = new Scene(root);
+        Scene scene = new Scene(root, 1624, 855);
         
         stage.setScene(scene);
+        stage.getIcons().add(new Image("/posdeskapp/images/point-of-sale-icon.png"));
+        stage.setTitle("POS");
+        stage.centerOnScreen();
         stage.show();
     }
 
