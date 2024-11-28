f0aac13c9e38fb102af18f2f7ee9868eef050175
Bright Issah <bphiri.aki@gmail.com>
2024-11-28 08:42:28 +0200

    WIP

---
+++ src/posdeskapp/models/PausedTransaction.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package posdeskapp.models;
+
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import posdeskapp.utils.POSHelper;
+
+/**
+ *
+ * @author biphiri
+ */
+public class PausedTransaction {
+
+    private final SimpleIntegerProperty pauseId;
+    private final SimpleStringProperty transactionDate;
+    private final SimpleDoubleProperty total;
+
+    public PausedTransaction(Integer pauseId, String transactionDate, Double total) {
+        this.pauseId = new SimpleIntegerProperty(pauseId);
+        this.transactionDate = new SimpleStringProperty(transactionDate);
+        this.total = new SimpleDoubleProperty(total);
+    }
+
+    public int getPauseId() {
+        return pauseId.get();
+    }
+
+    public String getTransactionDate() {
+        return transactionDate.get();
+    }
+
+    public double getTotal() {
+        return total.get();
+    }
+
+    public String getFormattedTransactionTotal() {
+        return POSHelper.formatValue(getTotal());
+    }
+
+}
