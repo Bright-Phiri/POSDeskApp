f72b023e883faa64cec28ca1e35b53ad64e8c370
Bright Issah <bphiri.aki@gmail.com>
2024-12-06 11:25:59 +0200

    WIP

---
+++ src/posdeskapp/models/InvoiceDetails.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package posdeskapp.models;
+
+import java.time.LocalDateTime;
+
+/**
+ *
+ * @author biphiri
+ */
+public class InvoiceDetails {
+    private String invoiceNumber;
+    private LocalDateTime invoiceDateTime;
+
+    public String getInvoiceNumber() {
+        return invoiceNumber;
+    }
+
+    public void setInvoiceNumber(String invoiceNumber) {
+        this.invoiceNumber = invoiceNumber;
+    }
+
+    public LocalDateTime getInvoiceDateTime() {
+        return invoiceDateTime;
+    }
+
+    public void setInvoiceDateTime(LocalDateTime invoiceDateTime) {
+        this.invoiceDateTime = invoiceDateTime;
+    }
+}
